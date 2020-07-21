fc9f1a67ab1d73962d1e990ff35de81b4160f317
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-22 00:22:52 +0700

    fhkg

--- p.php
+++ p.php
@@ -135,4 +135,3 @@ echo color("red","          Format Kode 08/62 loss karepmu\n");
                                         $debug['text'] = $pesan;
                                         $debug['respon'] = json_decode($datas, true);
 }
-// echo change()."\n";
